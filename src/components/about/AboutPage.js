f8efeacaf92fb33f2cff6f70769ac4598c780ab7
Bamboo <aravind.guna91@gmail.com>
2018-04-15 23:49:32 -0400

    'initcommit'

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class AboutPage extends React.Component{
+    render(){
+        return(
+            <div>
+                    <h1>About</h1>
+                    <p>This Application uses react , redux , react router and a varity of other helpful language</p>
+                     
+            </div>
+
+        );
+    }
+}
+
+export default AboutPage;
---
+++ src/components/home/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component{
+    render(){
+        return(
+            <div className="jumbotron">
+                    <h1>React Administration</h1>
+                    <p>React ,Redux and React Router in ES6 for ultra-reponsive web app </p>
+                    <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
+            </div>
+
+        );
+    }
+}
+
+export default HomePage;
